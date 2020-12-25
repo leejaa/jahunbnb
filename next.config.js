15986cd7c32865d706a721e5c02f0a1e91e5be64
이자훈 <leejahun0@gmail.com>
2020-12-25 22:20:00 +0900

    pwa setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -7,4 +12,4 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   distDir: 'build',
-};
+});
